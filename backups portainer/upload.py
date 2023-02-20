5706601b5646f9efebb177f9834a0f154f916e3b
maxens-git <61346038+maxens-git@users.noreply.github.com>
2023-02-20 17:47:00 +0100

    Update upload.py

--- backups portainer/upload.py
+++ backups portainer/upload.py
@@ -5,9 +5,9 @@ print("1. Connect to github")
 print("2. Upload to github")
 
 choix1 = input(" : ")
-if choix1 == 1:
+if choix1 == "1":
     os.system("sudo docker login ghcr.io -u maxens.verron+github@outlook.com")
-elif choix1 == 2:
+elif choix1 == "2":
     tag = input("Tag : ")
     os.system("sudo docker build . -t ghcr.io/maxens-git/backup/backup-portainer:" + tag)
     os.system("sudo docker push ghcr.io/maxens-git/backup/backup-portainer:" + tag)
