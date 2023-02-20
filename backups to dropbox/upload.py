903d5da03e5dd5db9162037a36340acde508da4f
maxens-git <61346038+maxens-git@users.noreply.github.com>
2023-02-20 17:42:21 +0100

    Update upload.py

--- backups to dropbox/upload.py
+++ backups to dropbox/upload.py
@@ -6,10 +6,10 @@ print("2. Upload to github")
 
 choix1 = input(" : ")
 if choix1 == 1:
-    os.system("docker login ghcr.io -u maxens.verron+github@outlook.com")
+    os.system("sudo docker login ghcr.io -u maxens.verron+github@outlook.com")
 elif choix1 == 2:
     tag = input("Tag : ")
-    os.system("docker build . -t ghcr.io/maxens-git/backup/backup:" + tag)
-    os.system("docker push ghcr.io/maxens-git/backup/backup:" + tag) 
+    os.system("sudo docker build . -t ghcr.io/maxens-git/backup/backup:" + tag)
+    os.system("sudo docker push ghcr.io/maxens-git/backup/backup:" + tag)
 else:
     print("Please choose 1 or 2")
