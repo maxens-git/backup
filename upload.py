18148be750a6f55244b6b65c76f3db2d3ea9097e
maxens-git <61346038+maxens-git@users.noreply.github.com>
2023-02-20 22:14:39 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -9,7 +9,7 @@ if choix1 == "1":
     os.system("sudo docker login ghcr.io -u maxens.verron+github@outlook.com")
 elif choix1 == "2":
     tag = input("Tag : ")
-    os.system("sudo docker build . -t ghcr.io/maxens-git/backup/backup-portainer:" + tag)
-    os.system("sudo docker push ghcr.io/maxens-git/backup/backup-portainer:" + tag)
+    os.system("sudo docker build . -t ghcr.io/maxens-git/backup:" + tag)
+    os.system("sudo docker push ghcr.io/maxens-git/backup:" + tag)
 else:
     print("Please choose 1 or 2")
