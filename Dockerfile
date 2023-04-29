267fd480b2d1c1e2c84527107354588e8d809096
maxens-git <61346038+maxens-git@users.noreply.github.com>
2023-04-29 19:38:53 +0200

    2.0.0

--- Dockerfile
+++ Dockerfile
@@ -1,57 +1,18 @@
 FROM alpine:latest
 
-# IMAGE ARGUMENTS PASSED IN FROM BUILDER
-ARG TARGETARCH
-ARG BUILDDATE
-ARG BUILDVERSION
-
-# set timezone from ENVs
-RUN export TZ=/usr/share/zoneinfo/${TZ}
-
-# INSTALL ADDITIONAL IMAGE DEPENDENCIES AND COPY APPLICATION TO IMAGE
 RUN apk update
-RUN apk add --no-cache \
-    sqlite \
-    curl \
-    bash \
-    tzdata \
-    openssl \
-    nano \
-    nodejs \
-    npm \
-    tzdata
+RUN apk add --no-cache rclone
+RUN apk add --no-cache sqlite
+RUN apk add --no-cache openssl
 
-#File for portainer
-RUN mkdir -p /src
-RUN mkdir -p /portainer
-COPY package.json /
-COPY src/*.js /src
+RUN mkdir /docker
 
-#File for dropbox
-COPY dropbox_uploader.sh /
-COPY backup.sh /
 COPY entrypoint.sh /
-COPY dropbox_uploader.sh /
+COPY backup.sh /
+
 WORKDIR /
-
-RUN chmod +x /entrypoint.sh && \
-    chmod +x /backup.sh && \
-    chmod +x /dropbox_uploader.sh
-
-VOLUME "/backup"
-
-RUN npm install --silent
-
-# DEFAULT ENV VARIABLE VALUES
-ENV PORTAINER_BACKUP_URL="http://portainer:9000"
-ENV PORTAINER_BACKUP_TOKEN=""
-ENV PORTAINER_BACKUP_DIRECTORY="/portainer"
-ENV PORTAINER_BACKUP_FILENAME="/portainer-backup.tar.gz"
-ENV PORTAINER_BACKUP_OVERWRITE=false
-ENV PORTAINER_BACKUP_CONCISE=false
-ENV PORTAINER_BACKUP_DEBUG=false
-ENV PORTAINER_BACKUP_DRYRUN=false
-ENV PORTAINER_BACKUP_STACKS=false
+RUN chmod +x entrypoint.sh
+RUN chmod +x backup.sh
 
 RUN echo "0 0 * * * /backup.sh" > /etc/crontabs/root
 
--- backup.sh
+++ backup.sh
@@ -3,41 +3,38 @@
 # create backup filename
 BACKUP_FILE="Bitwardenrs_$(date "+%F-%H%M%S")"
 BACKUP_FILE1="Adguard_Home_$(date "+%F-%H%M%S")"
-BACKUP_FILE2="Portainer_$(date "+%F-%H%M%S")"
 BACKUP_FILE3="Nginx_Proxy_Manager_$(date "+%F-%H%M%S")"
-
+BACKUP_FILE4="Minecraft-spigot_$(date "+%F-%H%M%S")"
 
 
 #Sauvegarde bitwarden
-sqlite3 /data/vaultwarden/db.sqlite3 ".backup '/tmp/db.sqlite3'"
-tar -czf - /tmp/db.sqlite3 /data/vaultwarden/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
-/dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE}.tar.gz /Bitwarden/${BACKUP_FILE}.tar.gz
+sqlite3 /docker/vaultwarden/db.sqlite3 ".backup '/tmp/db.sqlite3'"
+tar -czf - /tmp/db.sqlite3 /docker/vaultwarden/attachments | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE}.tar.gz
+rclone copy /tmp/${BACKUP_FILE}.tar.gz onedrive:Backups/vaultwarden
 echo "sauvegarde bitwarden terminée"
 
-#Sauvegarde NPM
-tar -czf - /data/npm | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE3}.tar.gz
-/dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE3}.tar.gz /Nginx_Proxy_Manager/${BACKUP_FILE3}.tar.gz
-echo "sauvegarde nginx proxy manager terminée"
-
-#Sauvegarde Portainer
-node /src/index.js backup
-
-tar -czf - /portainer | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE2}.tar.gz
-/dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE2}.tar.gz /Portainer/${BACKUP_FILE2}.tar.gz
-echo "sauvegarde portainer termimnée"
 
 #Sauvegarde Adguard Home
-tar -czf - /data/adguardhome | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE1}.tar.gz
-/dropbox_uploader.sh -f /config/.dropbox_uploader upload /tmp/${BACKUP_FILE1}.tar.gz /Adguard_Home/${BACKUP_FILE1}.tar.gz
+tar -czf - /docker/adguardhome | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE1}.tar.gz
+rclone copy /tmp/${BACKUP_FILE1}.tar.gz onedrive:Backups/adguardhome
 echo "sauvegarde adguard home terminée"
+
+
+
+#Sauvegarde NPM
+tar -czf - /docker/npm | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /tmp/${BACKUP_FILE3}.tar.gz
+rclone copy /tmp/${BACKUP_FILE3}.tar.gz onedrive:Backups/npm
+echo "sauvegarde nginx proxy manager terminée"
+
 
 
 
 # cleanup tmp folder
 rm -rf /tmp/*
-rm -rf /portainer/*
+
 
 # delete older backups if variable is set & greater than 0
 if [ ! -z $DELETE_AFTER ] && [ $DELETE_AFTER -gt 0 ]; then
   /deleteold.sh
 fi
+
